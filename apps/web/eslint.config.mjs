3787fc119600a882c44872e843fa41e4b3d1e8d7
Camilo Vargas <ca06vargas@gmail.com>
2025-03-21 14:53:52 -0500

    initialize project structure with API and web apps, add ESLint configuration, and set up basic README files

---
+++ apps/web/eslint.config.mjs
@@ -0,0 +1,23 @@
+import createConfig from '@honora/eslint-config/create-config';
+import pluginQuery from '@tanstack/eslint-plugin-query';
+
+export default createConfig(
+  {
+    react: true,
+  },
+  {
+    plugins: {
+      '@tanstack/query': pluginQuery,
+    },
+    rules: {
+      'antfu/top-level-function': 'off',
+      'unicorn/filename-case': [
+        'error',
+        {
+          case: 'kebabCase',
+          ignore: ['README.md', '~__root.tsx'],
+        },
+      ],
+    },
+  },
+);
